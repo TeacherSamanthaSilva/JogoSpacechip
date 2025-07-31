349e28f555dc88997b263745dd0308fb3753a539
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-07-31 16:17:44 -0300

    jogo spacechip
    nesse commit criei o jogo spacechip

--- scripts/menu.py
+++ scripts/menu.py
@@ -1,10 +1,32 @@
 import pygame
+from scripts.animatedbg import AnimatedBg
 from scripts.obj import Obj
 from scripts.scene import Scene
+from scripts.settings import *
+from scripts.text import Text
 
 class Menu(Scene):
 
     def __init__(self):
         super().__init__()
 
-        
+        self.bg = AnimatedBg("assets/menu/bg.png",[0,0],[0,-720],[self.all_sprites])
+        self.title = Text("assets/fonts/airstrike.ttf", 50,"SpaceShip 13k", "white", [448,288])
+        self.text_info = Text("assets/fonts/airstrike.ttf", 21,"Press Start To Play", "white", [508,513])
+    
+    def events(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                self.active = False
+        return super().events(event)
+
+    def update(self):
+
+        self.bg.update()
+        self.title.draw()
+        self.text_info.drawFade()
+        return super().update()
+
+
+
+
--- scripts/scene.py
+++ scripts/scene.py
@@ -1,5 +1,6 @@
 import pygame
 from scripts.obj import Obj
+from scripts.settings import *
 
 class Scene:
 
@@ -9,15 +10,38 @@ class Scene:
         self.all_sprites = pygame.sprite.Group()
         self.active = True
 
+        self.fade = Fade(5)
+
     def events(self, event):
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                self.active = False
+        pass
 
     def draw(self):
         self.all_sprites.draw(self.display)
 
     def update(self):
+        self.fade.draw()
         self.all_sprites.update()
+
+
+class Fade:
+
+    def __init__(self, speed):
+        
+        self.display = pygame.display.get_surface()
+        self.surface = pygame.Surface((WIDTH, HEIGHT)).convert_alpha()
+        self.surface.fill("black")
+
+        self.alpha = 255
+        self.speed = speed
+    
+    def draw(self):
+
+        if self.alpha > 0:
+            self.alpha -= self.speed
+
+        self.surface.set_alpha(self.alpha)
+        self.display.blit(self.surface, [0,0])
+
+
+
 
